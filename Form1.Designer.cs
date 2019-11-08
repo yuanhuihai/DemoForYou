1af6889322b8b32485e0903c584854f490f5ae0e
yuanhuihai <yuanhuihai@sina.com>
2019-11-08 10:19:12 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace DemoForYou
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.list_INFO = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.connect_server = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cmbServerName = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Server_IP = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.Button();
+            this.disconnect_server = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 179);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Channel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(148, 179);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Dev";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(298, 179);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Group";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 179);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 18);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "添加组";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(61, 177);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(71, 21);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "GAS";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(176, 177);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(85, 21);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "VT126";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(338, 177);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(78, 21);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "G1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 219);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "TagID";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(45, 217);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(276, 21);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.Text = "current";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(446, 217);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 18);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "添加变量";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // list_INFO
+            // 
+            this.list_INFO.FormattingEnabled = true;
+            this.list_INFO.ItemHeight = 12;
+            this.list_INFO.Location = new System.Drawing.Point(9, 346);
+            this.list_INFO.Margin = new System.Windows.Forms.Padding(2);
+            this.list_INFO.Name = "list_INFO";
+            this.list_INFO.Size = new System.Drawing.Size(556, 220);
+            this.list_INFO.TabIndex = 15;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(11, 251);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 18);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "同步读";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(11, 286);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(56, 18);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "异步读";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(263, 251);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(56, 18);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "同步写";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(341, 251);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(79, 21);
+            this.textBox7.TabIndex = 20;
+            this.textBox7.Text = "100";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(263, 286);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(56, 18);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "异步写";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button9.Location = new System.Drawing.Point(452, 286);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(86, 38);
+            this.button9.TabIndex = 22;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(681, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 12);
+            this.label7.TabIndex = 29;
+            // 
+            // connect_server
+            // 
+            this.connect_server.Location = new System.Drawing.Point(14, 99);
+            this.connect_server.Name = "connect_server";
+            this.connect_server.Size = new System.Drawing.Size(75, 23);
+            this.connect_server.TabIndex = 28;
+            this.connect_server.Text = "连接";
+            this.connect_server.UseVisualStyleBackColor = true;
+            this.connect_server.Click += new System.EventHandler(this.connect_server_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 58);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 12);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "OPC服务器列表";
+            // 
+            // cmbServerName
+            // 
+            this.cmbServerName.FormattingEnabled = true;
+            this.cmbServerName.Location = new System.Drawing.Point(14, 73);
+            this.cmbServerName.Name = "cmbServerName";
+            this.cmbServerName.Size = new System.Drawing.Size(339, 20);
+            this.cmbServerName.TabIndex = 26;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(102, 30);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 12);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "本机IP";
+            // 
+            // Server_IP
+            // 
+            this.Server_IP.Location = new System.Drawing.Point(150, 25);
+            this.Server_IP.Name = "Server_IP";
+            this.Server_IP.Size = new System.Drawing.Size(151, 21);
+            this.Server_IP.TabIndex = 24;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(12, 23);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 23;
+            this.Search.Text = "搜索";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // disconnect_server
+            // 
+            this.disconnect_server.Location = new System.Drawing.Point(127, 99);
+            this.disconnect_server.Name = "disconnect_server";
+            this.disconnect_server.Size = new System.Drawing.Size(75, 23);
+            this.disconnect_server.TabIndex = 30;
+            this.disconnect_server.Text = "连接";
+            this.disconnect_server.UseVisualStyleBackColor = true;
+            this.disconnect_server.Click += new System.EventHandler(this.disconnect_server_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 778);
+            this.Controls.Add(this.disconnect_server);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.connect_server);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cmbServerName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Server_IP);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.list_INFO);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox list_INFO;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button connect_server;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cmbServerName;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox Server_IP;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button disconnect_server;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpcDaHelper;
+using OPCDAAUTO;
+using System.Net;
+
+
+namespace DemoForYou
+{
+    public partial class Form1 : Form
+    {
+       
+
+        string strHostIP = "";
+        string strHostName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public OpcDAClientHelper opcClient = null;
+
+        //枚举本地OPC服务器
+        private void GetLocalServer()
+        {
+            //获取本地计算机IP,计算机名称
+            IPHostEntry IPHost = Dns.GetHostEntry(Environment.MachineName);
+            if (IPHost.AddressList.Length > 0)
+            {
+                strHostIP = IPHost.AddressList[2].ToString();
+                //txtRemoteServerIP.Text = IPHost.AddressList[2].ToString();
+                Server_IP.Text = IPHost.AddressList[2].ToString();
+            }
+            else
+            {
+                return;
+            }
+            //通过IP来获取计算机名称，可用在局域网内
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(strHostIP);
+            strHostName = ipHostEntry.HostName.ToString();
+            //获取本地计算机上的OPCServerName
+            try
+            {
+                //创建OPC服务器对象，获得OPC服务器列表
+
+                object serverList = OpcDAClientHelper.GetOpcServer(strHostIP);
+                //显示服务器列表
+                foreach (string turn in (Array)serverList)
+                {
+                    cmbServerName.Items.Add(turn);
+                    list_INFO.Items.Add("发现服务器 " + strHostIP.ToString() + ":  " + turn.ToString());
+                    if (list_INFO.Items.Count == 40)
+                    {
+                        list_INFO.Items.Clear();
+                    }
+                }
+                cmbServerName.SelectedIndex = 0;
+                connect_server.Enabled = true;
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("枚举本地OPC服务器出错：" + err.Message, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+        private void Search_Click(object sender, EventArgs e)
+        {
+            GetLocalServer();
+        }
+
+        //连接OPC服务器
+        private bool ConnectServer(string ServerIP, string ServerName)
+        {
+            try
+            {
+         
+                opcClient = new OpcDAClientHelper(ServerIP, ServerName);
+
+                if (opcClient.Connect())
+                {
+
+                    label3.Text = "OPC 已连接";
+                }
+                else
+                {
+                    MessageBox.Show("women");
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("连接远程服务器出现错误：" + err.Message, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+        private void connect_server_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //连接服务器
+                if (!ConnectServer(Server_IP.Text, cmbServerName.Text))
+                {
+                    return;
+                }
+                list_INFO.Items.Add("已连接到" + ":  " + cmbServerName.Text.ToString());
+                if (list_INFO.Items.Count == 40)
+                {
+                    list_INFO.Items.Clear();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("初始化出错：" + err.Message, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+        Group group = null;
+        private void button2_Click(object sender, EventArgs e)
+        {
+             group = new Group(textBox3.Text.Trim(), textBox4.Text.Trim(), textBox5.Text.Trim());
+            group.IsActive = true;
+            group.IsSubscribed = true;
+            group.DeadBand = 0;
+            group.UpdateRate = 10;
+            opcClient.AddGroup(group);
+            group.DataChangeEvent += group_DataChangeEvent;
+            group.AsyncReadCompleteEvent +=group_AsyncReadCompleteEvent;
+            group.AsyncWriteCompleteEvent +=group_AsyncWriteCompleteEvent;
+            MessageBox.Show("添加组成功");
+        }
+
+        private void group_AsyncWriteCompleteEvent(Item item)
+        {
+            MessageBox.Show("item:"+item.Name+ "异步写完成");
+        }
+
+        private void group_AsyncReadCompleteEvent(Item item)
+        {
+            MessageBox.Show("异步读结果:tag:"+item.Name+",value:"+item.Value.ToString()+"");
+        }
+
+        private void group_DataChangeEvent(Item item)
+        {
+            list_INFO.Invoke(new Action(() => { list_INFO.Items.Insert(0,string.Format(
+            "item:{0},value:{1},time:{2},quality:{3}",item.GetItemId(),item.Value.ToString(),item.Timesnamp,item.Quality)); }));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            group.AddItem(new Item(textBox6.Text) { IsActive =true});
+            MessageBox.Show("添加Item成功");
+        }
+
+  
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+             group.Items[0].Read();
+             MessageBox.Show(group.Items[0].Value.ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int tmp;
+            group.AsyncRead(new List<Item>() {group.Items[0]}, 1, out tmp);
+            MessageBox.Show("cancleID:"+tmp.ToString());
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            group.Items[0].ObjValue = this.textBox7.Text.Trim();
+            if (group.Items[0].Write())
+            {
+                MessageBox.Show("写成功");
+            }
+            else
+
+            {
+                MessageBox.Show("写失败");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int tmp;
+            group.Items[0].ObjValue = this.textBox7.Text.Trim();
+            group.AsyncWrite(new List<Item>() { group.Items[0] }, 1, out tmp);
+            MessageBox.Show("cancleID:" + tmp.ToString());
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void disconnect_server_Click(object sender, EventArgs e)
+        {
+            opcClient.DisConnect();
+        }
+    }
+}
